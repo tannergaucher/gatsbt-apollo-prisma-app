b33a5894563fa96c974491d5dd7adcd89f3f5134
tanner gaucher <tannermichaelgaucher@gmail.com>
2019-02-22 00:07:30 +0800

    .

--- frontend/src/components/PleaseSignIn.js
+++ frontend/src/components/PleaseSignIn.js
@@ -1,27 +1,24 @@
 import React from 'react'
 import { Query } from 'react-apollo'
 
-import Layout from './layout'
 import { CURRENT_USER_QUERY } from './User'
 import Signin from './Signin'
 
 const PleaseSignIn = props => (
-  <Layout>
-    <Query query={CURRENT_USER_QUERY}>
-      {({ data, loading }) => {
-        if (loading) return <p>Loading </p>
-        if (!data.me) {
-          return (
-            <>
-              <h3>You must be signed in for that</h3>
-              <Signin />
-            </>
-          )
-        }
-        return props.children
-      }}
-    </Query>
-  </Layout>
+  <Query query={CURRENT_USER_QUERY}>
+    {({ data, loading }) => {
+      if (loading) return <p>Loading </p>
+      if (!data.me) {
+        return (
+          <>
+            <h3>You must be signed in for that</h3>
+            <Signin />
+          </>
+        )
+      }
+      return props.children
+    }}
+  </Query>
 )
 
 export default PleaseSignIn
